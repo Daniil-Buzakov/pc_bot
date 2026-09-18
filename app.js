aedb06f4aa16bbb556fce9546e73715ead423749
Daniil-Buzakov <dbuzakov@internet.ru>
2026-09-18 15:45:58 +1000

    Update app.js

--- app.js
+++ app.js
@@ -98,4 +98,4 @@ catalogEl.addEventListener('click', async (e) => {
 });
 
 // Заменяем URL бэкенда (в реальном проекте — через переменную окружения)
-const BACKEND_URL = 'https://your-backend.railway.app';
+const BACKEND_URL = 'pcbot-production-7c6d.up.railway.app';
